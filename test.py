1421717c0ae63107a99f6464a7967491307e3b05
Jerin John <jerin.john@totalticketing.com>
2024-09-05 13:34:51 +0530

    added test cases for Delimiters can be of any length in []

--- test.py
+++ test.py
@@ -6,43 +6,53 @@ from main import add, AddHandlerException
 class TestAddFunction:
 
     def test_add_function_three_digits(self):
-        response = add('//,\n1,2,3')
+        response = add('//[,]\n1,2,3')
         assert response == 6
 
     def test_add_function_empty_string(self):
-        response = add('//,\n')
+        response = add('//[,]\n')
         assert response == 0
 
     def test_add_function_signle_digit(self):
-        response = add('//,\n3')
+        response = add('//[,]\n3')
         assert response == 3
 
 
     @pytest.mark.skip(reason="Skipping this test for now")
     def test_add_with_different_delimiters(self):   # ignoring passing case of \n anymore
-        response = add('//,\\n\n1\n2,3')
+        response = add('//[,][\n]\n1\n2,3')
         assert response == 6
 
     def test_support_multiple_delimieters(self):
-        response = add('//;\n1;2;3')
+        response = add('//[;]\n1;2;3')
         assert response == 6
 
     def test_handle_invalid_syntax_input(self):
         with pytest.raises(AddHandlerException, match="Improply created input pattern"):
-            add('/;\n124')
+            add('/[;]\n124')
 
         with pytest.raises(AddHandlerException, match="Improply created input pattern"):
-            add('//;123')
+            add('//[;]123')
+
+        with pytest.raises(AddHandlerException, match="Improply created input pattern"):
+            add('//;\n1;2;3')
+
 
     def test_handle_negative_numebr(self):
         with pytest.raises(AddHandlerException, match="negative numbers not allowed -1"):
-            add('//;\n-1;2;3')
+            add('//[;]\n-1;2;3')
 
 
     def test_handle_negative_numebrs(self):
         with pytest.raises(AddHandlerException, match="negative numbers not allowed -1,-3"):
-            add('//;\n-1;2;-3')
+            add('//[;]\n-1;2;-3')
 
     def test_prevent_4_digit_numbers(self):
-        response = add('//;\n1;2;1000;3')
+        response = add('//[;]\n1;2;1000;3')
         assert response == 6
+
+
+    def test_delimiter_length_more_than_1(self):
+        response = add('//[;]\n1;2;1000;3')
+        assert response == 6
+
