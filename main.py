e57834f833410aca7305113864ab42bfe0c818c8
Jerin John <jerin.john@totalticketing.com>
2024-09-05 12:54:20 +0530

    Witten logic in add function to run test cases

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ description: interview @ incubyte
 dated: Sept 5, 2024
 """
 
+
 def add(string):
     """ Get sum of comma seperated digits in the string  """
 
-    return
+    delimiters = ','
 
+    numbers = string.split(delimiters)
+    out = 0
+    for item in numbers:
+       if item.isdigit():
+           out += int(item)
+
+    return out
