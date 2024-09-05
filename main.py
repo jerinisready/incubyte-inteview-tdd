8fd67a39b2625835f850cec0f7cc49e5b6066793
Jerin John <jerin.john@totalticketing.com>
2024-09-05 13:01:01 +0530

    added multiple delimiters using regex

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ author : @jerinisready
 description: interview @ incubyte
 dated: Sept 5, 2024
 """
+import re
 
 
 def add(string):
     """ Get sum of comma seperated digits in the string  """
 
-    delimiters = ','
+    delimiters = [',', '\n']
+    delimiters_regex = '[{}]'.format(''.join(delimiters))
 
-    numbers = string.split(delimiters)
+    numbers = re.split(delimiters_regex, string)
     out = 0
     for item in numbers:
        if item.isdigit():
