929025cac78d8692c026133f220f2842d80c8779
Jerin John <jerin.john@totalticketing.com>
2024-09-05 12:41:28 +0530

    initialized files and created a base function

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+author : @jerinisready
+description: interview @ incubyte
+dated: Sept 5, 2024
+"""
+
+def add(string):
+    """ Get sum of comma seperated digits in the string  """
+
+    return
+
