0628115a3db6f7733a2df1823e9a9bab421e5d83
Jerin John <jerin.john@totalticketing.com>
2024-09-05 12:46:13 +0530

    added TTD INIT test case and kept initial test_report.log

---
+++ test.py
@@ -0,0 +1,20 @@
+import pytest
+
+from main import add
+
+
+class TestAddFunction:
+
+    def test_add_function_three_digits(self):
+        response = add('1,2,3')
+        assert response == 6
+
+    def test_add_function_empty_string(self):
+        response = add('')
+        assert response == 0
+
+    def test_add_function_signle_digit(self):
+        response = add('3')
+        assert response == 3
+
+
